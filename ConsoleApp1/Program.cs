2f9208d1a3f42802dbf6901a0c06e7d0946629ef
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 12:40:59 +0200

    pierwszy

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Strona 1";
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("uno");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Dos");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("tres");
+            Console.ResetColor();
+            Console.Write("Naciśnij dowolny przycisk, żeby przejść do następnej strony");
+            Console.ReadKey();
+            Console.Clear();
+            Console.Title = "Druga strona";
+            Console.Write("Linia 4");
+            Console.WriteLine("Linia 5");
+            Console.Write("Naciśnij dowolny przycisk, żeby zamknąć okno");
+        }
+    }
+}
